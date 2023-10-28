71e44ec6a60cbbc9a28072f0f31191c74a5f726b
javakanaya <javakanaya@gmail.com>
2023-10-28 22:04:21 +0700

    feat: queries for get ticket

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Airport;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TicketController extends Controller
 {
@@ -12,7 +14,43 @@ class TicketController extends Controller
      */
     public function index()
     {
-        return Ticket::all();
+        // return Ticket::all();
+        $source_airport_id = 2;
+        $destination_airport_id = 3;
+        $depature_date = '2023-11-03';
+        $userClassChoice = 4;
+        $countTicket = 1;
+        $classType = '';
+
+        switch ($userClassChoice) {
+            case 1:
+                $classType = "first";
+                break;
+            case 2:
+                $classType = "business";
+                break;
+            case 3:
+                $classType = "premium_economy";
+                break;
+            case 4:
+                $classType = "economy";
+                break;
+            default:
+                $classType = "economy";
+        }
+
+
+        $ticket = DB::table('tickets')
+            ->join('routes', 'tickets.route_id', '=', 'routes.id')
+            ->where('routes.source_airport_id', '=', $source_airport_id)
+            ->where('routes.destination_airport_id', '=', $destination_airport_id)
+            ->join('seats', 'routes.seat_id', '=', 'seats.id')
+            ->whereDate('routes.departure', $depature_date)
+            ->where('seats.' . $classType, '>', $countTicket)
+            ->get();
+
+        return $ticket;
+
     }
 
     /**
