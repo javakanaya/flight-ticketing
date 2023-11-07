9866c149add8e5c9e1143f68b1ef724f73095e28
andrianang <andrianangg@gmail.com>
2023-11-08 00:05:00 +0700

    main update

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,28 @@
+import Navbar from "../Components/Navbar";
+import Hero from "../Components/Hero";
+import GetTicket from "../Components/GetTicket";
+import "../../css/home.css"
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+            <Hero 
+                cName="hero"
+                heroImg=""
+                title={<>
+                    Travel Anywhere <br />
+                    Less Worry
+                  </>}
+                text="Find hotel and ticket with best experience"
+            />
+            <div className="img">
+
+            </div>
+            <GetTicket />
+            <div className="h-60 bg-white"></div>
+        </>
+    )
+}
+
+export default Home;
