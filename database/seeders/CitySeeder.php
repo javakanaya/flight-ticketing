c307a1308944b7444f5287f5f9b40267accca83b
Java Kanaya Prada <javakanaya@gmail.com>
2023-11-27 01:08:22 +0700

    update: city and province seeders

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -14,19 +14,18 @@ class CitySeeder extends Seeder
      
     public function run(): void
     {
-        //
-        $indonesia_cities = [
-            ["id" => 3372, "name" => "Solo", "province_id" => 33],
-            ["id" => 3401, "name" => "KULON PROGO", "province_id" => 34],
-            ["id" => 3578, "name" => "Surabaya", "province_id" => 35],
-            ["id" => 3671, "name" => "Jakarta", "province_id" => 36],
-            ["id" => 5171, "name" => "Denpasar", "province_id" => 51],
-            ["id" => 1275, "name" => "Medan", "province_id" => 12],
-            ["id" => 3273, "name" => "Bandung", "province_id" => 32],
-            ["id" => 7371, "name" => "Makassar", "province_id" => 73],
-            ["id" => 1471, "name" => "Pekanbaru", "province_id" => 1471],
-        ];
+        $json = file_get_contents(database_path('seeders/json/city.json'));
+        $cities = json_decode($json, true);
 
-        DB::table('cities')->insert($indonesia_cities);
+        $payload = [];
+        foreach ($cities as $city) {
+            
+            $payload[] = [
+                'id' => $city['id'],
+                'province_id' => $city['province_id'],
+                'name' => $city['name']
+            ];
+        }
+        DB::table('cities')->insert($payload);
     }
 }
