592d473cf6c1f0dbf3b0c5b8f6a091ddb2f1b2ac
Java Kanaya Prada <javakanaya@gmail.com>
2023-11-28 10:51:13 +0700

    fix: typo

--- resources/js/Pages/Admin/Routes/Index.jsx
+++ resources/js/Pages/Admin/Routes/Index.jsx
@@ -16,7 +16,7 @@ const Index = ({ flightRoutes, auth }) => {
                     Routes
                     <Link
                         href={ route('admin.routes.create') }
-                        class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md"
+                        className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md"
                     >
                         + ADD
                     </Link>
