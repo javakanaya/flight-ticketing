dd692b94ea56f892f9f4eb24cd3dbc498878e7ce
Java Kanaya Prada <javakanaya@gmail.com>
2023-11-30 14:34:17 +0700

    feat: redesign profile page

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/profile/transactions', [ProfileController::class, 'transaction'])->name('profile.transaction');
+    Route::get('/profile/bookings', [ProfileController::class, 'transaction'])->name('profile.transaction');
 });
 
 Route::middleware(['auth', 'admin'])->group(function () {
